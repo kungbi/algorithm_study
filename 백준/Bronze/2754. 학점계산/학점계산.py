920685ca2c7e567d03a3a74edab1e1ad14353f5c
Shin Woongbi <kipper12335@gmail.com>
2023-10-23 13:19:42 +0900

    [Bronze V] Title: 학점계산, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2754. 학점계산/학점계산.py
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -1,25 +1,16 @@
 
 str = input()
 
-
 def to_number(str):
-    score = 0
-    if (str[0] == 'A'):
-        score += 4.0
-    elif (str[0] == 'B'):
-        score += 3.0
-    elif (str[0] == 'C'):
-        score += 2.0
-    elif (str[0] == 'D'):
-        score += 1.0
-    elif (str[0] == 'F'):
+    score = 4.0 - (ord(str[0]) - ord('A'))
+    if (score < 0):
         return (0.0)
-
     if (str[1] == '+'):
         score += 0.3
     elif (str[1] == '-'):
         score -= 0.3
+    if (score < 0):
+        score = 0.0
     return (score)
-
 
 print(to_number(str))
