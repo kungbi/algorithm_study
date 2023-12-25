88d1572e8413e50085f97bb610b8b89e399563b0
Shin Woongbi <kipper12335@gmail.com>
2023-12-25 21:42:04 +0900

    [level 3] Title: N으로 표현, Time: 0.02 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.py
@@ -0,0 +1,26 @@
+
+def calc_sets(a_set, b_set):
+    tmp_set = set()
+    for a in a_set:
+        for b in b_set:
+            tmp_set.add(a - b)
+            tmp_set.add(a + b)
+            tmp_set.add(a * b)
+            if b != 0:
+                tmp_set.add(a / b)
+    return tmp_set
+
+def solution(N, number):
+    sets = list()
+    sets.append(set())
+    for i in range(1, 9):
+        tmp_set = set()
+        tmp_set.add(int(str(N) * i))
+        for j in range(1, i):
+            tmp_set = tmp_set.union(calc_sets(sets[i - j], sets[j - i]))
+        sets.append(tmp_set)
+        for j in tmp_set:
+            if j == number:
+                return i
+            
+    return -1
