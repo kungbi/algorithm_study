580c588b5c98761dee494bda300ca44c5067320b
Shin Woongbi <dobby12335@gmail.com>
2024-01-23 21:06:16 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.23 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
@@ -0,0 +1,24 @@
+def solution(bandage, health, attacks):
+    attacks.reverse()
+    
+    t = 0
+    max_health = health
+    heal_cnt = damage_cnt = attack_damage = 0
+    while 0 < health and attacks:
+        if attacks[-1][0] == t:
+            attack_damage = attacks.pop()[1]
+            health -= attack_damage
+            heal_cnt = 0
+        else:
+            heal_cnt += 1
+            health += bandage[1]
+            if heal_cnt == bandage[0]:
+                health += bandage[2]
+                heal_cnt = 0
+            if max_health < health:
+                health = max_health
+        t += 1
+        
+    if 0 < health:
+        return health
+    return -1
