4c8c6801877d2d9972b389c0c1557614c0966347
Shin Woongbi <dobby12335@gmail.com>
2024-01-26 17:41:55 +0900

    [Bronze II] Title: Hashing, Time: 216 ms, Memory: 17760 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -4,13 +4,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int l = sc.nextInt();
+        long l = sc.nextInt();
         String str = sc.next();
 
-        int hash = 0;
-        for(int i = 0; i < str.length(); i++){
-            hash += (str.charAt(i) - 'a' + 1) * Math.pow(31, i);
+        long hash = 0;
+        long r = 1;
+        int a;
+        for(int i = 0; i < l; i++){
+            a = str.charAt(i) - 'a' + 1;
+            hash = (hash + (a * r) % 1234567891) % 1234567891;
+            r = (r * 31) % 1234567891;
         }
-        System.out.println(hash % 1234567891);
+        System.out.println(hash);
     }
 }
