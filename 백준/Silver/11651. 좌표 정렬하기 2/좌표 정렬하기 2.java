6e1fe62a45305ce44311387e090ab12eb289a0f9
Shin Woongbi <dobby12335@gmail.com>
2024-01-26 22:17:08 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 1636 ms, Memory: 68372 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -1,46 +1,31 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
 
-public class Main {
-    static class Pair implements Comparable<Pair>{
-        int x;
-        int y;
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-        public Pair(int x, int y) {
-            this.x = x;
-            this.y = y;
+        int[][] arr = new int[n][2];
+        for(int i=0; i<arr.length; i++){
+            String[] line = br.readLine().split(" ");
+            arr[i][0] = Integer.parseInt(line[0]);
+            arr[i][1] = Integer.parseInt(line[1]);
         }
 
-        @Override
-        public int compareTo(Pair o) {
-            if (this.y != o.y) {
-                return this.y - o.y;
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[1] == o2[1])
+                    return o1[0] - o2[0];
+                return o1[1] - o2[1];
             }
+        });
 
-            return this.x - o.x;
-        }
-
-        @Override
-        public String toString() {
-            return String.format("%d %d", this.x, this.y);
-        }
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-        List<Pair> arr = new ArrayList<>();
-
-        int x, y;
-        for(int i = 0; i < n; i++){
-            x = sc.nextInt();
-            y = sc.nextInt();
-            arr.add(new Pair(x, y));
-        }
-
-        Collections.sort(arr);
-        for(Pair pair : arr){
-            System.out.println(pair.toString());
-        }
+        for(int[] a : arr)
+            System.out.println(a[0] + " " + a[1]);
     }
 }
