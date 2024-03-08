38c87249fde8bbdf198f45d66919ae58a8975a4f
Shin Woongbi <dobby12335@gmail.com>
2024-03-08 14:09:08 +0900

    [Gold III] Title: 사다리 조작, Time: 7112 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/15684. 사다리 조작/사다리 조작.py
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.py
@@ -1,42 +1,59 @@
-n, m, h = map(int, input().split())
-line = [[0]*(n+1) for _ in range(h+1)]
-for _ in range(m):
-    x, y = map(int, input().split())
-    line[x][y] = 1
-    line[x][y+1] = -1
-    
-def check(data):
-    for k in range(1, n+1):
-        j = k
-        for i in range(1, h+1):
-            j += data[i][j]
-        if j != k:
-            return False
-    return True
+import sys
+sys.setrecursionlimit(10**6)
 
-def dfs(cnt, idx):
-    global min_cnt
-    if check(line):
-        min_cnt = min(min_cnt, cnt)
-        return
-    elif cnt == 3 or min_cnt <= cnt:
-        return
-    for i in range(idx, length):
-        if not line[grid[i][0]][grid[i][1]] and not line[grid[i][0]][grid[i][1]+1]:
-            line[grid[i][0]][grid[i][1]] = 1
-            line[grid[i][0]][grid[i][1]+1] = -1
-            dfs(cnt+1, i + 1)
-            line[grid[i][0]][grid[i][1]] = 0
-            line[grid[i][0]][grid[i][1]+1] = 0
-    
-grid = []
-for i in range(1, h+1):
-    for j in range(1, n):
-        if not line[i][j] and not line[i][j+1]:
-            grid.append((i, j))
-length, min_cnt = len(grid), 1e9
-dfs(0, 0)
-if min_cnt <= 3:
-    print(min_cnt)
-else:
-    print(-1)
+def input():
+	return sys.stdin.readline().rstrip()
+
+def is_end(line_arr):
+	for x in range(n):
+		start = x
+		for y in range(h):
+			x += line_arr[y][x]
+		if start != x:
+			return False
+	return True
+
+n = 0
+h = 0
+result = 0
+def f(line_arr, pos_list, pos_list_len, depth, index):
+	global result
+
+	if is_end(line_arr):
+		result = min(result, depth)
+		return
+	if 3 <= depth or result <= depth:
+		return
+	for i in range(index, pos_list_len):
+		a, b = pos_list[i]
+		if not line_arr[a][b] and not line_arr[a][b + 1]:
+			line_arr[a][b] = 1
+			line_arr[a][b + 1] = -1
+			f(line_arr, pos_list, pos_list_len, depth + 1, i + 1)
+			line_arr[a][b] = 0
+			line_arr[a][b + 1] = 0
+
+def solution():
+	global n, m, h, result
+
+	n, m, h = map(int, input().split())
+	line_arr = [[0] * n for _ in range(h)]
+	for _ in range(m):
+		a, b = map(int, input().split())
+		line_arr[a - 1][b - 1] = 1
+		line_arr[a - 1][b] = -1
+
+	pos_list = []
+	pos_list_len = 0
+	for a in range(h):
+		for b in range(n - 1):
+			if line_arr[a][b] == 0 and line_arr[a][b + 1] == 0:
+				pos_list.append((a, b))
+				pos_list_len += 1
+	
+	result = 4
+	f(line_arr, pos_list, pos_list_len, 0, 0)
+	print(result if result < 4 else -1)
+	
+
+solution()
