6b538791d67233ff5edbc053410fa5e2d55459aa
Woongbi Shin <dobby12335@gmail.com>
2024-03-10 17:42:26 +0900

    [level 3] Title: 광고 삽입, Time: 51.62 ms, Memory: 14.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/72414. 광고 삽입/광고 삽입.py
@@ -0,0 +1,55 @@
+def time2sec(time):
+    h, m, s = map(int, time.split(':'))
+    sec = h * 3600 + m * 60 + s
+    return sec
+    
+
+def solution(play_time, adv_time, logs):
+    arr = [0] * (time2sec(play_time) + 2)
+    
+    for log in logs:
+        start, end = log.split('-')
+        start_i = time2sec(start)
+        end_i = time2sec(end)
+        arr[start_i] += 1
+        arr[end_i] -= 1
+    
+    for i in range(1, len(arr)):
+        arr[i] = arr[i] + arr[i - 1]
+        
+    for i in range(1, len(arr)):
+        arr[i] = arr[i] + arr[i - 1]
+    
+    max_v = 0                           # 5
+    max_t = 0                          
+    adv_s = time2sec(adv_time)
+    for i in range(adv_s - 1, len(arr)):
+        if i >= adv_s:
+            if max_v < arr[i] - arr[i - adv_s]:
+                max_v = arr[i] - arr[i - adv_s]
+                max_t = i - adv_s + 1
+        else:
+            if max_v < arr[i]:
+                max_v = arr[i]
+                max_t = i - adv_s + 1
+
+    
+    # 최대값 찾기
+    # adv_s = time2sec(adv_time)
+    # max_t = -1
+    # max_v = -1
+    # for i in range(1, len(arr) - adv_s):
+    #     tmp = arr[i + adv_s] - arr[i - 1]
+    #     if i == 1:
+    #         tmp = arr[i + adv_s]
+    #         i -= 1
+    #     if max_v < tmp:
+    #         max_v = tmp
+    #         max_t = i
+
+    h = max_t // (60 * 60)
+    m = (max_t % (60 * 60)) // 60
+    s = (max_t % (60 * 60)) % 60
+    return(str(h).zfill(2) + ':' + str(m).zfill(2) + ':' + str(s).zfill(2))
+    
+    
