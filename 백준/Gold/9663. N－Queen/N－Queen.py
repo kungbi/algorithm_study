1b84f8cc10304d53fe72b45c920caa2cd227a53e
Woongbi Shin <dobby12335@gmail.com>
2024-03-10 22:20:02 +0900

    [Gold IV] Title: N-Queen, Time: 14452 ms, Memory: 148996 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,48 +1,46 @@
 import sys
 
+
 def input():
-	return sys.stdin.readline().rstrip()
+    return sys.stdin.readline().rstrip()
 
-def is_frame(x, y, n):
-	return 0 <= x < n and 0 <= y < n
 
-def is_possible(arr, pos):
-	x, y = pos
-	for pos_queen in arr:
-		q_x, q_y = pos_queen
-		if x == q_x or y == q_y:
-			return False
-		if abs(x - q_x) == abs(y - q_y):
-			return False
-	return True
+def is_possible(arr, x, y):
+    for col in range(x):
+        if arr[col] == y:
+            return False
+        if abs(arr[col] - y) == abs(col - x):
+            return False
+    return True
 
-def f(arr, y, n):
-	global visited_col
 
-	if y == n:
-		return 1
+def f(arr, visited, x):
+    if x == n:
+        return 1
 
-	cnt = 0
-	for x in range(n):
-		if visited_col[x] == True:
-			continue
+    cnt = 0
+    for y in range(n):
+        if visited[y] == False:
+            if is_possible(arr, x, y):
+                visited[y] = True
+                arr[x] = y
+                cnt += f(arr, visited, x + 1)
+                visited[y] = False
 
-		if is_possible(arr, (x, y)):
-			arr.append((x, y))
-			visited_col[x] = True
-			cnt += f(arr, y + 1, n)
-			arr.pop()
-			visited_col[x] = False
-		
-	return cnt
+    return cnt
 
-visited_col = []
+
+n = 0
+
+
 def solution():
-	global visited_col
+    global n
 
-	n = int(input())
-	arr = []
-	visited_col = [False] * n
-	print(f(arr, 0, n))
+    n = int(input())
+    arr = [-1] * n
+    visited = [False] * n
+    result = f(arr, visited, 0)
+    print(result)
+
 
 solution()
