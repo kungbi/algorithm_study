d71ca650ecc7f0c347c2ecdfba22b6d95d5359bd
Woongbi Shin <dobby12335@gmail.com>
2024-03-10 22:53:10 +0900

    [Gold IV] Title: 알파벳, Time: 6588 ms, Memory: 173392 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -0,0 +1,53 @@
+import sys
+
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+
+dxs = [-1, 0, 1, 0]
+dys = [0, -1, 0, 1]
+
+
+def is_frame(x, y):
+    return 0 <= x < c and 0 <= y < r
+
+
+def to_index(c):
+    return ord(c) - ord("A")
+
+
+def dfs(arr, curr, visited):
+    global result
+
+    x, y, d = curr
+    result = max(result, d)
+    for dx, dy in zip(dxs, dys):
+        nx = x + dx
+        ny = y + dy
+        if is_frame(nx, ny) == False:
+            continue
+
+        index = to_index(arr[ny][nx])
+        if visited[index] == False:
+            visited[index] = True
+            dfs(arr, (nx, ny, d + 1), visited)
+            visited[index] = False
+
+
+r = c = 0
+
+
+def solution():
+    global r, c, result
+    r, c = map(int, input().split())
+    arr = [list(input()) for _ in range(r)]
+
+    result = 0
+    visited = [False] * 26
+    visited[to_index(arr[0][0])] = True
+    dfs(arr, (0, 0, 1), visited)
+    print(result)
+
+
+solution()
