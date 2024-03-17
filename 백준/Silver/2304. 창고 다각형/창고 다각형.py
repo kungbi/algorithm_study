4d66e0a4e94c24d78b9d72b737161bbeb6e9a233
Woongbi Shin <dobby12335@gmail.com>
2024-03-18 02:31:03 +0900

    [Silver II] Title: 창고 다각형, Time: 64 ms, Memory: 34068 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.py
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.py
@@ -1,4 +1,5 @@
 import sys
+from collections import defaultdict
 
 
 def input():
@@ -7,44 +8,29 @@ def input():
 
 def solution():
     n = int(input())
-    arr = [[0] * 1001 for _ in range(1001)]
+    columns = defaultdict(int)
 
-    max_h = 0
     max_l = 0
+    max_h = 0
     for _ in range(n):
         l, h = map(int, input().split())
+        columns[l] = h
         if max_h < h:
-            max_l = l
             max_h = h
-        for j in range(h + 1):
-            arr[j][l] = 1
+            max_l = l
+    result = max_h
 
-    result = max_h  # 가장 긴 기둥의 넓이
-    left_i = max_l - 1
-    right_i = max_l + 1
+    num = 0
+    for x in range(1, max_l):
+        if num < columns[x]:
+            num = columns[x]
+        result += num
 
-    for h in range(max_h, -1, -1):
-        left_i_tmp = 0
-        right_i_tmp = 1000
-
-        # left_i에서 왼쪽으로 탐색
-        while left_i_tmp <= left_i and arr[h][left_i_tmp] != 1:
-            left_i_tmp += 1
-
-        # right_i에서 오른쪽으로 탐색
-        while right_i <= right_i_tmp and arr[h][right_i_tmp] != 1:
-            right_i_tmp -= 1
-
-        # 만약 발견하면 해당 높이에서 발견한 위치의 곱을 더하기
-        if left_i_tmp <= left_i:
-            width = abs(left_i - left_i_tmp) + 1
-            result += width * h
-            left_i = left_i_tmp - 1
-        if right_i <= right_i_tmp:
-            width = abs(right_i - right_i_tmp) + 1
-            result += width * h
-            right_i = right_i_tmp + 1
-
+    num = 0
+    for x in range(1000, max_l, -1):
+        if num < columns[x]:
+            num = columns[x]
+        result += num
     print(result)
 
 
