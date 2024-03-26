b1e20254fb7bb010556626bcd0a64b7ba7a19292
Woongbi Shin <dobby12335@gmail.com>
2024-03-26 13:16:43 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 596.45 ms, Memory: 95.1 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.py
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.py
@@ -1,27 +1,27 @@
-from collections import defaultdict
+MAX = 1_000_000
+
 
 def solution(edges):
-    result = [0, 0, 0, 0]
-    n = 0
-    inout_map = dict()
+    # [1, 0] 1나가기 0들어옴.
+    inout_arr = [[0, 0] for _ in range(MAX + 1)]
+    
+    max_i = -1
+    max_v = 0
     for edge in edges:
-        if edge[0] not in inout_map:
-            inout_map[edge[0]] = [0, 0]
-        if edge[1] not in inout_map:
-            inout_map[edge[1]] = [0, 0]
-        
-        inout_map[edge[0]][0] += 1
-        inout_map[edge[1]][1] += 1
+        a, b = edge
+        inout_arr[a][0] += 1
+        inout_arr[b][1] += 1
+
+    a_i = 0
+    a = b = c = 0
+    for i in range(len(inout_arr)):
+        if 1 < inout_arr[i][0] and inout_arr[i][1] == 0: # 정점
+            a = inout_arr[i][0]
+            a_i = i
+        elif inout_arr[i][0] == 0 and inout_arr[i][1] != 0: # 막대
+            b += 1
+        elif inout_arr[i][0] == 2 and 2 <= inout_arr[i][1]: # 도넛
+            c += 1
     
-    for v, inout in inout_map.items():
-        if 2 <= inout[0] and inout[1] == 0:
-            result[0] = v
-            n = inout[0]
-        elif inout[0] == 2 and 2 <= inout[1]:
-            result[3] += 1
-        elif inout[0] == 0 and inout[1]:
-            result[2] += 1
-    result[1] = n - result[2] - result[3]
-    return result
-    
-    
+    a -= b + c
+    return [a_i, a, b, c]
