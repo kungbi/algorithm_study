9cfc2ae1b4351029043afa899d8b86fa84a4d312
Woongbi Shin <dobby12335@gmail.com>
2024-04-21 10:12:30 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -1,6 +1,8 @@
 def f(n, a, b, count):
-    if (a - 1) // 2 + 1 == (b - 1) // 2 + 1:
-        return count + 1
+    # if (a - 1) // 2 + 1 == (b - 1) // 2 + 1:
+    #     return count + 1
+    if a == b:
+        return count
     return f(n // 2, (a - 1) // 2 + 1, (b - 1) // 2 + 1, count + 1)
     
 
