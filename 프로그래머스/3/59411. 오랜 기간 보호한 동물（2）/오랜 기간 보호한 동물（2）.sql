328ca02f94e21a3a17482960179787329a726f4e
Woongbi Shin <dobby12335@gmail.com>
2024-04-27 13:57:56 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -0,0 +1,4 @@
+SELECT      ins.ANIMAL_ID, ins.NAME
+FROM        ANIMAL_INS ins JOIN ANIMAL_OUTS outs ON ins.ANIMAL_ID = outs.ANIMAL_ID
+ORDER BY    (outs.DATETIME - ins.DATETIME) DESC
+LIMIT       2
