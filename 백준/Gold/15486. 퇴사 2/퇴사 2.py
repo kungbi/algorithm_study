8a8123de4274c4b4c4d86ad22d4423550439fed7
Woongbi Shin <dobby12335@gmail.com>
2024-06-04 15:55:24 +0900

    [Gold V] Title: 퇴사 2, Time: 2892 ms, Memory: 346424 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.py
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -1,7 +1,4 @@
 import sys
-from heapq import heappush
-from heapq import heappop
-from collections import deque
 
 
 def input():
@@ -12,26 +9,16 @@ def solution():
     n = int(input())
     work_list = [list(map(int, input().split())) for _ in range(n)]
     dp = [0] * (n + 1)
-    answer = 0
-    heap = []
     prev_max = 0
 
     for i in range(n):
-        end_idx = i + work_list[i][0] - 1
+        prev_max = max(prev_max, dp[i])
 
-        if n <= end_idx:
-            continue
+        end_idx = i + work_list[i][0]
+        if end_idx <= n:
+            dp[end_idx] = max(dp[end_idx], prev_max + work_list[i][1])
 
-        while heap and heap[0][0] < i:
-            prev_max = max(prev_max, heap[0][1])
-            heappop(heap)
-
-        dp[end_idx] = max(dp[end_idx], prev_max + work_list[i][1])
-        heappush(heap, (end_idx, dp[end_idx]))
-
-        answer = max(answer, dp[end_idx])
-
-    print(answer)
+    print(max(dp))
 
 
 solution()
