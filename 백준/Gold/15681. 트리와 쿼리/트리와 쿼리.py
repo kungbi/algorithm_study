2080e987dc3a4fad0724342871ebc21da51fd8f0
Woongbi Shin <dobby12335@gmail.com>
2025-04-03 11:07:47 +0900

    [Gold V] Title: 트리와 쿼리, Time: 404 ms, Memory: 77752 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.py
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.py
@@ -1,24 +1,46 @@
 import sys
-sys.setrecursionlimit(1000000000) #반복제한 늘리기
-N,R,Q=map(int,sys.stdin.readline().split(' '))
+
+sys.setrecursionlimit(10**6)
 
 
-m=[[]for _ in range(N+1)]
-visit=[-1 for _ in range(N+1)]
+def input():
+    return sys.stdin.readline().rstrip()
 
-for _ in range(N-1):
-    a,b=map(int,sys.stdin.readline().split(' '))
-    m[a].append(b)
-    m[b].append(a)
 
-def dfs(now):
-    global visit
-    visit[now]=1#나 자신을 추가해준다
-    for i in m[now]:
-        if visit[i]==-1: #방문하지 않은 방문 가능 노드가 있다면
-            visit[now]+=dfs(i) #방문하며 그 노드의 서브트리 개수를 더해준다
-    return visit[now] #내 서브트리 개수를 리턴한다
-dfs(R)
-for _ in range(Q):
-    get=int(sys.stdin.readline())
-    print(visit[get])
+def solution(tree, node, dp, visited):
+    visited[node] = True
+    if dp[node] != -1:
+        return dp[node]
+
+    sum_num = 1
+    for n_node in tree[node]:
+        if visited[n_node] == True:
+            continue
+        sum_num += solution(tree, n_node, dp, visited)
+
+    dp[node] = sum_num
+    return dp[node]
+
+
+def main():
+    n, r, q = map(int, input().split())
+
+    tree = dict()
+    for _ in range(n - 1):
+        u, v = map(int, input().split())
+        if not u in tree:
+            tree[u] = []
+        if not v in tree:
+            tree[v] = []
+        tree[u].append(v)
+        tree[v].append(u)
+
+    dp = [-1] * (n + 1)
+    visited = [False] * (n + 1)
+    solution(tree, r, dp, visited)
+    for _ in range(q):
+        node = int(input())
+        print(dp[node])
+
+
+main()
