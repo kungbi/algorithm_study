f2f6e66db38f5366fbdb7d3cd5c2d7225388d4d6
Woongbi Shin <dobby12335@gmail.com>
2025-07-29 14:17:34 +0900

    [Silver II] Title: 상자넣기, Time: 88 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1965. 상자넣기/상자넣기.py
@@ -0,0 +1,21 @@
+import sys
+
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+
+def main():
+    n = int(input())
+    arr = list(map(int, input().split()))
+
+    dp = [1] * n
+    for i in range(n):
+        for j in range(i + 1, n):
+            if arr[i] < arr[j]:
+                dp[j] = max(dp[j], dp[i] + 1)
+
+    print(max(dp))
+
+
+main()
