3cdb3334482a85724af443d6c1ab6c2d30244469
Woongbi Shin <dobby12335@gmail.com>
2025-08-21 18:49:02 +0900

    [Gold V] Title: 2의 멱수의 합, Time: 1796 ms, Memory: 88352 KB -BaekjoonHub

--- 백준/Gold/2410. 2의 멱수의 합/2의 멱수의 합.py
+++ 백준/Gold/2410. 2의 멱수의 합/2의 멱수의 합.py
@@ -5,16 +5,9 @@ def input():
     return sys.stdin.readline().rstrip()
 
 
-def is_power(n):
-    while 1 < n:
-        if n % 2 == 1:
-            return False
-        n //= 2
-    return True
-
-
 def main():
     n = int(input())
+
     dp = [0] * (n + 1)
     dp[0] = 1
 
@@ -22,10 +15,8 @@ def main():
     while i <= n:
         for j in range(i, n + 1):
             dp[j] += dp[j - i]
-            dp[j] %= 1_000_000_000
         i *= 2
-    # print(dp)
-    print(dp[n])
+    print(dp[n] % 1_000_000_000)
 
 
 main()
