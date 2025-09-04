d33ce0a9bfacd8c51f69ea98a7f99f6cd15ab4e3
Woongbi Shin <dobby12335@gmail.com>
2025-09-04 11:33:40 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 36 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.py
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.py
@@ -5,27 +5,22 @@ def input():
     return sys.stdin.readline().rstrip()
 
 
-def f(dp):
-    dp[1][1] = 1
-    dp[2][1] = 1
-    dp[2][2] = 1
-    dp[3][1] = 1
-    dp[3][2] = 1
-    dp[3][3] = 1
-    for i in range(4, 10_000 + 1):
-        dp[i][1] = dp[i - 1][1]
-        dp[i][2] = dp[i - 2][1] + dp[i - 2][2]
-        dp[i][3] = dp[i - 3][1] + dp[i - 3][2] + dp[i - 3][3]
+MAX = 10_000
 
 
-def solution():
-    dp = [[0] * 4 for _ in range(10_000 + 1)]
-    f(dp)
+def main():
+    dp = [1] * (MAX + 1)
+    dp[2] = 2
+    for i in range(3, MAX + 1):
+        dp[i] += dp[i - 2]
+
+    for i in range(3, MAX + 1):
+        dp[i] += dp[i - 3]
 
     t = int(input())
     for _ in range(t):
         n = int(input())
-        print(sum(dp[n]))
+        print(dp[n])
 
 
-solution()
+main()
