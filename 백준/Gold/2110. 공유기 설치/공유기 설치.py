a66be00f973931d8dae0de52dc5ee0af758464cf
Woongbi Shin <dobby12335@gmail.com>
2025-09-15 09:11:56 +0900

    [Gold IV] Title: 공유기 설치, Time: 236 ms, Memory: 41704 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -5,31 +5,32 @@ def input():
     return sys.stdin.readline().rstrip()
 
 
-def solution():
+def main():
     n, c = map(int, input().split())
     arr = [int(input()) for _ in range(n)]
     arr.sort()
 
-    left = 0
-    right = arr[-1] - arr[0]
+    low = 1
+    high = arr[-1] - arr[0]
     answer = 0
-    while left <= right:
-        mid = (left + right) // 2
 
+    while low <= high:
+        mid = (low + high) // 2
         count = 1
-        curr = arr[0]
-        for i in range(1, n):
-            if curr + mid <= arr[i]:
-                curr = arr[i]
+        last = arr[0]
+
+        for pos in arr[1:]:
+            if mid <= pos - last:
                 count += 1
+                last = pos
 
         if c <= count:
-            left = mid + 1
             answer = mid
+            low = mid + 1  # 더 크게 가능?
         else:
-            right = mid - 1
+            high = mid - 1
 
     print(answer)
 
 
-solution()
+main()
