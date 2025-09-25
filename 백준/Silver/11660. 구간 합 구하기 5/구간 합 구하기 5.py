9e93a059fab953ceb33170dad741c914f3eff190
Woongbi Shin <dobby12335@gmail.com>
2025-09-25 20:56:28 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 716 ms, Memory: 106240 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -0,0 +1,30 @@
+import sys
+
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+
+def main():
+    n, m = map(int, input().split())
+    arr = [[0] * (n + 1)] + [[0] + list(map(int, input().split())) for _ in range(n)]
+    prefix_sum = [[0] * (n + 1) for _ in range(n + 1)]
+
+    for y in range(1, n + 1):
+        for x in range(1, n + 1):
+            prefix_sum[y][x] = (
+                prefix_sum[y - 1][x]
+                + prefix_sum[y][x - 1]
+                + arr[y][x]
+                - prefix_sum[y - 1][x - 1]
+            )
+
+    for _ in range(m):
+        y1, x1, y2, x2 = map(int, input().split())
+        tmp = (
+            prefix_sum[y2][x1 - 1] + prefix_sum[y1 - 1][x2] - prefix_sum[y1 - 1][x1 - 1]
+        )
+        print(prefix_sum[y2][x2] - tmp)
+
+
+main()
